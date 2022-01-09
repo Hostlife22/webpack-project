3ccc555f96f3089c26f41f0e8097c62843913b40
Hostlife21 <sima.serafim2@mail.ru>
2022-01-10 00:24:13 +0300

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import sword from '../images/swc-sword.png';
+import swordSvg from '../images/sword.svg';
 import '../styles/index.scss';
 import Recipes from './Recipes';
 
@@ -9,9 +11,10 @@ const App = () => {
         <section>
           <h1>Oh Hai, React</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
+        <Recipes />
       </main>
-
-      <Recipes />
     </>
   );
 };
